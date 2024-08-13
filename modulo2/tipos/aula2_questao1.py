369fa38b834395c84c6bbd16db03cd3672658302
Ricardo-PDBD <ricardo.navarro@pdbomdespacho.com.br>
2024-08-13 15:42:44 -0300

    Add files via upload

---
+++ modulo2/tipos/aula2_questao1.py
@@ -0,0 +1,16 @@
+"""
+1) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+Armazene o valor 5 em uma variável e o valor 2 em outra variável
+Imprima o tipo de dados dessas duas variáveis
+Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+Imprima a terceira variável e também o seu tipo
+
+"""
+
+valor1 = 5
+valor2 = 2
+print(type(valor1))
+print(type(valor2))
+r = valor1 / valor2
+print(r)
+print(type(r))
---
+++ modulo2/tipos/aula2_questao2.py
@@ -0,0 +1,20 @@
+"""
+2) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+Some os valores da segunda e terceira variável e armazene em outra variável.
+Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+"""
+texto = "o sesultado é "
+valor1 = 10
+valor2 = 3.5
+r = valor1 + valor2
+
+print(texto)
+print(type(texto))
+print(valor1)
+print(type(valor1))
+print(valor2)
+print(type(valor2))
+print(r)
+print(type(r))
