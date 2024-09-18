5e007f2cc1f78ef1f0fd312d57b6e5e0ba0d73e8
Ricardo-PDBD <ricardo.navarro@pdbomdespacho.com.br>
2024-09-18 15:36:31 -0300

    Delete modulo7/strings directory

--- modulo7/strings/test.py
+++
@@ -1,6 +0,0 @@
-numero = input("Digite o celular: ")
-if len(numero) == 8:
-    numero_formatado = "9" + numero
-
-numero_final = f"{numero_formatado[:5]}-{numero_formatado[5:]}"
-print("Numero formatado:", numero_final)
